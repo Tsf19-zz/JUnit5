1247a46604b5e5139808a1d22034cd0e0858506a
Tousif <md.tousifmemon@gmail.com>
2019-03-21 15:49:51 +0530

    LifeCycleHook and TestInstance added

---
+++ src/test/java/JavaBrains/_1_JUnit5Basics/_4_DisplayNameAndDisabled.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package JavaBrains._1_JUnit5Basics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Tousif
+ *
+ */
+public class _4_DisplayNameAndDisabled {
+
+	
+	_1_MathUtils mathUtils;
+	
+	@BeforeEach
+	void init() {
+		System.out.println("@BeforeEach - init()..");
+	}
+
+	/**
+	 * @DisplayName is used to declare a custom display name for the annotated test class or test method.
+	 */
+	@Test
+	@DisplayName("Addition Method") 
+	void testAdd() {
+		int expected = 14;
+		int actual = mathUtils.add(7, 7);
+		assertEquals(expected, actual, "The add() method should add two numbers");
+	}
+	
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),"Divide by zero throws ArithmeticException");
+	}
+	
+	
+	@Test
+	void testComputeCircleArea() {
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return circle area");
+	}
+	
+}
